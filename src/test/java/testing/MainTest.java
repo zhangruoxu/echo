4e267c653569491f47af734a4a79502b415da388
Yifei Zhang <yzhang@cse.unsw.edu.au>
2017-10-31 15:05:18 +1100

    test(testing): add a test case

--- src/test/java/testing/MainTest.java
+++ src/test/java/testing/MainTest.java
@@ -1,6 +1,11 @@
 package testing;
 
+import java.util.List;
+
 import org.junit.Test;
+
+import util.Config;
+import util.ManifestParser;
 
 public class MainTest {
 	@Test
@@ -21,5 +26,19 @@ public class MainTest {
 	@Test
 	public void test3() {
 		Main.main(new String[] { "-app", "0", "1", "-emulator", "Nexus_5_API_19"});
+	}
+	
+	@Test
+	public void test4() {
+		Config.init(null);
+		String[] args = new String[] {"-app", "0", "-emulator", "Nexus_5_API_19" };
+		TestingOptions.v().processOptions(args);
+		List<String> appPaths = TestingOptions.v().getAppPaths();
+		for(String appPath : appPaths)
+			Main.testingApp(appPath, (s, d) -> {
+				String pkgName = ManifestParser.getPackageName(appPath);
+				d.closeApp();
+				d.removeApp(pkgName);
+			});
 	}
 }
