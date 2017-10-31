cd62e957b9cb3f4ca3e5fed6455bcf7fe03d5163
Yifei Zhang <yzhang@cse.unsw.edu.au>
2017-10-31 15:02:58 +1100

    feat(util): obtain app pakckage name by a static method

--- src/main/java/util/ManifestParser.java
+++ src/main/java/util/ManifestParser.java
@@ -1,5 +1,6 @@
 package util;
 
+import java.io.File;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -49,6 +50,12 @@ public class ManifestParser {
 	// Return the package name of an app
 	public String getPackageName() {
 		return manifest.getPackageName();
+	}
+	
+	public static String getPackageName(String appPath) {
+		if(! new File(appPath).isFile())
+			return null;
+		return new ManifestParser(appPath).getPackageName();
 	}
 	
 	// Return the main activities
