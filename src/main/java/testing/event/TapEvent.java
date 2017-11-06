5c4ecafe97b28c2020c20c4dd23a044760e66acf
Yifei Zhang <nwpuzhangyifei@gmail.com>
2017-11-06 16:12:02 +1100

    feat(testing): add a class TapEvent to represents tapping on the screen

---
+++ src/main/java/testing/event/TapEvent.java
@@ -0,0 +1,43 @@
+package testing.event;
+
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import util.PointF;
+
+/**
+ * This class represents touch events from the touchscreen.
+ * 
+ * This class is adapted from the class MonkeyTouchEvent of the Android Open Source Project.
+ * 
+ * @author yifei
+ */
+public class TapEvent extends MotionEvent {
+
+	public TapEvent(long downAt, int metaState) {
+		super(Event.EVENT_TYPE_TOUCH, downAt, metaState);
+		assert mFromPointers.size() == 1;
+		PointF p = mFromPointers.get(0);
+		mX = Math.round(p.x);
+		mY = Math.round(p.y);
+	}
+
+	/**
+	 * Inject a touch event. Touch a coordinate then waiting for a short period of time.
+	 */
+	@Override
+	public void injectEvent(AndroidDriver<AndroidElement> driver) {
+		new TouchAction(driver).tap(mX, mY).waitAction(Throttle.v().getDuration()).perform();
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("[TouchEvent] Touch the point ( ").append(mX).append(", ").append(mY).append(").");
+		return builder.toString();
+	}
+	
+	// (mX,mY) indicates the touching coordinate
+	private int mX;
+	private int mY;
+}
