fd4414e23e8627d6a5d916d106184ee69756b6f2
Yifei Zhang <nwpuzhangyifei@gmail.com>
2017-11-08 09:33:35 +1100

    test(testing): add several test cases

--- src/test/java/testing/random/TestRandomEventSource.java
+++ src/test/java/testing/random/TestRandomEventSource.java
@@ -6,12 +6,19 @@ import org.junit.Test;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.android.AndroidKeyCode;
 import testing.Main;
 import testing.TestingOptions;
+import testing.event.DragEvent;
 import testing.event.Event;
+import testing.event.KeyEvent;
+import testing.event.TapEvent;
 import testing.event.Throttle;
+import testing.event.ThrottleEvent;
+import testing.event.inspect.CheckActivityEvent;
 import util.AppInfoWrapper;
 import util.Config;
+import util.PointF;
 
 public class TestRandomEventSource {
 	@Test
@@ -25,6 +32,78 @@ public class TestRandomEventSource {
 				System.out.println(e);
 				e.injectEvent(info, d);
 			}
+		});
+	}
+	
+	/**
+	 * Test end call key. This key makes the device sleep.
+	 */
+	@Test
+	public void test2() {
+		initTesting("0", (info, d) -> {
+			new KeyEvent(AndroidKeyCode.KEYCODE_ENDCALL).injectEvent(info, d);
+		});
+	}
+	
+	/**
+	 * Test menu and home key.
+	 */
+	@Test
+	public void test3() {
+		initTesting("0", (info, d) -> {
+			new KeyEvent(AndroidKeyCode.MENU).injectEvent(info, d);
+			new KeyEvent(AndroidKeyCode.HOME).injectEvent(info, d);
+		});
+	}
+	
+	/**
+	 * Test tap event
+	 */
+	@Test
+	public void test4() {
+		initTesting("0", (info, d) -> {
+			System.out.println("# Height: " + d.manage().window().getSize().getHeight());
+			System.out.println("# Width: " + d.manage().window().getSize().getWidth());
+			new TapEvent(-1, -1).addPointer(0, new PointF(1, 1775)).injectEvent(info, d);
+		});
+	}
+	
+	/**
+	 * Test drag event
+	 */
+	@Test
+	public void test5() {
+		initTesting("0", (info, d) -> {
+			new DragEvent(-1, -1).addFromTo(0, new PointF(398, 1388),  new PointF(365, 1371)).injectEvent(info, d);
+		});
+	}
+	
+	/**
+	 * Test relaunch the testing app during testing
+	 * 
+	 * The app will be relaunched, rather than be resumed.
+	 */
+	@Test
+	public void test6() {
+		initTesting("0", (info, d) -> {
+//			Throttle.v().init(500);
+//			new KeyEvent(AndroidKeyCode.KEYCODE_DPAD_CENTER).injectEvent(info, d);
+//			new ThrottleEvent().injectEvent(info, d);
+//			new CheckActivityEvent().injectEvent(info, d);
+//			new ThrottleEvent().injectEvent(info, d);
+//			new KeyEvent(AndroidKeyCode.KEYCODE_CONTACTS).injectEvent(info, d);
+//			new ThrottleEvent().injectEvent(info, d);
+//			new CheckActivityEvent().injectEvent(info, d);
+//			new ThrottleEvent().injectEvent(info, d);
+			
+			Throttle.v().init(1000);
+			new KeyEvent(AndroidKeyCode.KEYCODE_DPAD_CENTER).injectEvent(info, d);
+			new ThrottleEvent().injectEvent(info, d);
+			new KeyEvent(AndroidKeyCode.KEYCODE_HOME).injectEvent(info, d);
+			new ThrottleEvent().injectEvent(info, d);
+			new TapEvent(-1, -1).addPointer(0, new PointF(900, 1600)).injectEvent(info, d);
+			new ThrottleEvent().injectEvent(info, d);
+			new CheckActivityEvent().injectEvent(info, d);
 		});
 	}
 	
